81790438c73af8fde56c7ff41809d2c0959f610b
guoqiang1204 <99319904+guoqiang1204@users.noreply.github.com>
2022-02-14 19:47:45 +0800

    pull commit

--- src/main/java/com/javasm/git/GitTest.java
+++ src/main/java/com/javasm/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hot fix text");
         System.out.println("master text");
         System.out.println("push text");
+        System.out.println("gitHub text");
     }
 }
